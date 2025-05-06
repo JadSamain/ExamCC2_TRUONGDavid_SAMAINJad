dbbb964c63291d70e3c784d640e374ddca87308a
JadS <151746572+JadSamain@users.noreply.github.com>
2025-05-06 11:14:17 +0200

    Update TagCounter.py

--- TagCounter.py
+++ TagCounter.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
@@ -11,15 +12,12 @@ class TagCount(MRJob):
 
     def mapper_get_tags(self, _, line):
         try:
-            # Diviser la ligne en colonnes
             userID, movieID, tag, timestamp = line.split(',')
-            # Émettre le tag et la valeur 1
             yield tag, 1
         except Exception:
             pass
 
     def reducer_count_tags(self, tag, counts):
-        # Somme des occurrences pour chaque tag
         yield tag, sum(counts)
 
 if __name__ == '__main__':
